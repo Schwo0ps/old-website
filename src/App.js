d80171ba06b745b2380a7640f57c2a3018366838
Nikita Kosolobov <nkosolobov2@gmail.com>
2023-02-21 15:03:15 -0800

    Add temporary warning sign
    Show terrible-looking temporary warning sign to tell
    people that the website is not yet ready...

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Header from "./Header";
 import Map from "./Map";
 import Timeline from "./Timeline";
+import { Alert, AlertTitle } from "@mui/material";
+import { CalendarMonth } from "@mui/icons-material";
 // import Contact from "./Contact";
 // import Footer from "./Footer";
 
 const theme = createTheme();
 
+function WaitingStub() {
+  return (
+    <Alert variant="filled" severity="warning">
+      <AlertTitle>Website is still under development</AlertTitle>
+      <CalendarMonth/> Please check back in 2 weeks <CalendarMonth/>
+    </Alert>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <br />
+      <WaitingStub />
+      <br />
       <hr />
       <Map />
       <hr />
